d46d0b1a107681b04addbac600ddf23db269c702
ting830812 <ting830812@gmail.com>
2020-09-23 12:04:54 +0800

    Add secret_key

--- covid19-web/covid19/covid19/settings.py
+++ covid19-web/covid19/covid19/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '{{ secret_key }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
